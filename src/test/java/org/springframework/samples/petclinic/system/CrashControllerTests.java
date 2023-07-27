8e45a8629dc593d0e83564936ca6788540f40882
Dave Syer <dsyer@vmware.com>
2023-07-27 11:35:48 +0000

    Copy system tests from main repo

--- src/test/java/org/springframework/samples/petclinic/system/CrashControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/system/CrashControllerTests.java
@@ -16,62 +16,31 @@
 
 package org.springframework.samples.petclinic.system;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.validation.BindException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import jakarta.servlet.http.HttpServletRequest;
 
 /**
  * Test class for {@link CrashController}
  *
  * @author Colin But
+ * @author Alex Lutz
  */
-// Waiting https://github.com/spring-projects/spring-boot/issues/5574
-@Disabled
-@WebMvcTest(controllers = CrashController.class)
+// Waiting https://github.com/spring-projects/spring-boot/issues/5574 ..good
+// luck ((plain(st) UNIT test)! :)
 class CrashControllerTests {
 
-	@Autowired
-	private MockMvc mockMvc;
+	CrashController testee = new CrashController();
 
 	@Test
 	void testTriggerException() throws Exception {
-		mockMvc.perform(get("/oups")).andExpect(view().name("exception"))
-				.andExpect(model().attributeExists("exception")).andExpect(forwardedUrl("exception"))
-				.andExpect(status().isOk());
-	}
+		RuntimeException thrown = assertThrows(RuntimeException.class, () -> {
+			testee.triggerException();
+		});
 
-	@ControllerAdvice
-	static class MockMvcValidationConfiguration {
-
-		private final BasicErrorController errorController;
-
-		MockMvcValidationConfiguration(BasicErrorController errorController) {
-			this.errorController = errorController;
-		}
-
-		@ExceptionHandler({ MethodArgumentNotValidException.class, BindException.class })
-		ResponseEntity<?> defaultErrorHandler(HttpServletRequest request, Exception ex) {
-			request.setAttribute("javax.servlet.error.request_uri", request.getPathInfo());
-			request.setAttribute("javax.servlet.error.status_code", 400);
-			return errorController.error(request);
-		}
-
+		assertEquals("Expected: controller used to showcase what happens when an exception is thrown",
+				thrown.getMessage());
 	}
 
 }
